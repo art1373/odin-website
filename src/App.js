8dd255a7b455426373ff06ba2e9c35e3f81fede4
alirezatk <alirezatk73@gmail.com>
2020-11-29 16:21:21 +0300

    feat: ui nav bar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.scss";
 
+import { NavBar } from "./components";
+
 function App() {
   return (
     <>
       <Router>
+        <NavBar />
         <Switch>
           <Route path="/" exact component={null} />
         </Switch>
